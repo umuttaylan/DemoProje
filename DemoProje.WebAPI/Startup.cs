fba593f028d1ac18a815120c34f1d5c6de8d4700
Umut Taylan KAYA <taylan.kaya@atez.com.tr>
2020-08-05 16:50:22 +0300

    Added swagger

--- DemoProje.WebAPI/Startup.cs
+++ DemoProje.WebAPI/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 
 namespace DemoProje.WebAPI
 {
@@ -26,6 +27,13 @@ namespace DemoProje.WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            #region Swagger
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo Proje Api", Version = "v1" });
+            });
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +51,12 @@ namespace DemoProje.WebAPI
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Demo Proje Web Api");
+            });
         }
     }
 }
